f95f202af60a52dd8d8f5074cd9996a111116c06
LordBohne <bohnelord@gmail.com>
2020-11-26 16:11:33 +0100

    The 3 base models
    Made a prototype of the 3 models needed to contain a whole table that has been queried. Not sure if it works yet.
    I may have forgotten to include every property that is needed.

---
+++ DatabaseManagementUI/Models/DataRowModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DatabaseManagementUI.Models
+{
+    public class DataRowModel
+    {
+        public List<DataFieldModel> DataRow { get; set; }
+        public string PrimaryKey { get; set; }
+    }
+}
---
+++ DatabaseManagementUI/Models/DataTableModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DatabaseManagementUI.Models
+{
+    class DataTableModel
+    {
+        public string TableName { get; set; }
+        public List<DataRowModel> dataRows { get; set; }
+    }
+}
