1ecd3d95b43ba948826876d83154810df2cdc638
LordBohne2 <lakr99@live.de>
2020-11-29 17:08:12 +0100

    ProcessStart

---
+++ DatabaseManagementUI/Models/SQL/StartSQL.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using System.Diagnostics;
+
+
+//namespace DatabaseManagementUI.Models.SQL
+namespace DatabaseManagementUI
+{
+    public class SQL
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="processStart"></param>
+        public void ProcessStart(string processStart)
+        {
+            //Simple-DMS\DatabaseManagementUI\XAMPP\mysql_start.bat
+
+            Process.Start(processStart);
+        }
+    }
+}
