8ef9f8f7ce453f13791f72b14e88c2efb4b1c0d0
LordBohne <bohnelord@gmail.com>
2020-12-12 10:45:51 +0100

    (probably) Finished work on the semicolon in string detection logic
    I probably finished work on the Semicolon within string of string detection logic. Seems to be working but it's probably way to converluded and ineffiecient. If anyone has a better Idea feel free to change the code.

--- DatabaseManagementUI/Models/DatabaseConnector.cs
+++ DatabaseManagementUI/Models/DatabaseConnector.cs
@@ -21,7 +21,7 @@ namespace DatabaseManagementUI.Models
         /// 1 beeing Mysql
         /// 2 beeing SQL Server
         /// </param>
-        public DatabaseConnector(string ConnectionString,int ServerType=1)
+        public DatabaseConnector(string ConnectionString, int ServerType = 1)
         {
             switch (ServerType)
             {
@@ -36,13 +36,13 @@ namespace DatabaseManagementUI.Models
                     {
                         throw e;
                     }
-                    
+
                 default:
                     break;
             }
-            
+
         }
-        public int ExecuteScript(string Script,string Delimiter=null)
+        public int ExecuteScript(string Script, string Delimiter = null)
         {
             try
             {
@@ -58,34 +58,63 @@ namespace DatabaseManagementUI.Models
 
                 throw;
             }
-            
+
         }
         public async void Query(string SQL)
         {
-            try
+
+            List<int> StringSymbolLocation = new List<int>();
+            List<int> SemicolonLocation = new List<int>();
+            for (int i = 0; i < SQL.Length; i++)
             {
-                List<int> StringSymbolLocation = new List<int>();
-                List<int> SemicolonLocation = new List<int>();
-                for (int i = 0; i < SQL.Length; i++)
+                if (SQL[i] == '\"' || SQL[i] == '\'' || SQL[i] == '`')
                 {
-                    if (SQL[i] == '\"' || SQL[i] == '\'' || SQL[i] == '`')
+                    StringSymbolLocation.Add(i);
+                }
+                else if (SQL[i] == ';')
+                {
+                    SemicolonLocation.Add(i);
+                }
+            }
+            // TODO: See if semucolonLocation is between the 2 closed StringSymbolLocations
+            List<int> SemicolonLocationInString = new List<int>();
+            bool firstrun = true;
+            for (int i = 0; i < SemicolonLocation.Count; i++)
+            {
+                for (int x = 0; x < StringSymbolLocation.Count; x++)
+                {
+                    if (SemicolonLocation[i] !< StringSymbolLocation[x] && firstrun)
                     {
-                        StringSymbolLocation.Add(i);
+                        var StringIndexBeforeSemicolon = StringSymbolLocation[x - 1];
+                        StringSymbolLocation.RemoveAt(x - 1);
+                        var StringIndexAfterSemicolon = StringSymbolLocation[x-1];
+                        StringSymbolLocation.RemoveAt(x-1);
+                        if (SemicolonLocation[i] >= StringIndexBeforeSemicolon && SemicolonLocation[i] <= StringIndexAfterSemicolon)
+                        {
+                            SemicolonLocationInString.Add(SemicolonLocation[i]);
+                        }
+                        firstrun = false;
                     }
-                    else if (SQL[i] == ';')
+                    else if (SemicolonLocation[i]! < StringSymbolLocation[x])
                     {
-                        SemicolonLocation.Add(i);
+                        var StringIndexBeforeSemicolon = StringSymbolLocation[x];
+                        StringSymbolLocation.RemoveAt(x);
+                        var StringIndexAfterSemicolon = StringSymbolLocation[x];
+                        StringSymbolLocation.RemoveAt(x);
+                        if (SemicolonLocation[i] >= StringIndexBeforeSemicolon && SemicolonLocation[i] <= StringIndexAfterSemicolon)
+                        {
+                            SemicolonLocationInString.Add(SemicolonLocation[i]);
+                        }
                     }
                 }
-                // TODO: See if semucolonLocation is between the 2 closed StringSymbolLocations
-                var sqlCommand = new MySqlCommand(SQL, MySqlConn);
-                
             }
-            catch (Exception)
+            foreach (var item in SemicolonLocationInString)
             {
-
-                throw;
+                MessageBox.Show(SQL.Substring(item));
             }
+            var sqlCommand = new MySqlCommand(SQL, MySqlConn);
+
+
         }
     }
 }
