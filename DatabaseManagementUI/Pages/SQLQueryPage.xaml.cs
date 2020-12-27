f7f62a6303c9dde2ef8d4a5ffbf5757aec5d1341
LordBohne <bohnelord@gmail.com>
2020-12-27 13:23:58 +0100

    Fixed the Issue that prevented the DataGrid from correctly displaying the Data

--- DatabaseManagementUI/Pages/SQLQueryPage.xaml.cs
+++ DatabaseManagementUI/Pages/SQLQueryPage.xaml.cs
@@ -30,26 +30,11 @@ namespace DatabaseManagementUI.Pages
         {
             Models.DatabaseConnector databaseConnector = new Models.DatabaseConnector(Models.DatabaseConnector.GenerateMySQLConnectionString("localhost", "root", "nordwind"));
             var test = databaseConnector.Query("SELECT * FROM ARTIKEL").Result;
-            ReturnedData.ItemsSource = test.AsDataView();
-        https://www.google.com/search?client=firefox-b-d&q=datagrid+bind+to+datatable
+            ReturnedData.ItemsSource = test.DefaultView;
+        //https://www.google.com/search?client=firefox-b-d&q=datagrid+bind+to+datatable
             //ViewModels.DatabaseViewModel databaseViewModel = new ViewModels.DatabaseViewModel("localhost", "root", "nordwind");
             //databaseViewModel.ExecuteAndRemodelQuery("SELECT * FROM ARTIKEL");
 
-            //DataGridTextColumn textColumn = new DataGridTextColumn();
-            //textColumn.Header = "First Name";
-            //textColumn.Binding = new Binding("Test");
-            //ReturnedData.Columns.Add(textColumn);
-            //var test = new List<string>();
-            //test.Add("TEST");
-            //test.Add("TEST");
-            //test.Add("TEST");
-            //ReturnedData.Items.Add(new MyClass { Test = test});
-            //ReturnedData.Items.Add(new MyClass { test = databaseConnector.Query(SQLInput.Text).Result.AsEnumerable() });
-            //MessageBox.Show("yay");
-        }
-        class MyClass
-        {
-            public List<string> Test { get; set; }
         }
     }
 }
